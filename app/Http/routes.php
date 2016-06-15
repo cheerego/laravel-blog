41bf922c3f0895c6f5d0e6729421543a7c94b550
placeless <41974873@qq.com>
2016-06-15 14:09:33 +0800

    end

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,20 +22,18 @@ Route::get('blog/findArticlesByCategory/{id}','IndexController@findarticlesbycat
 
 Route::auth();
 // Authentication Routes...
-Route::get('login', 'Auth\AuthController@showLoginForm');
-
+//Route::get('login', 'Auth\AuthController@showLoginForm');
 //Route::post('login', 'Auth\AuthController@login');
 //Route::get('logout', 'Auth\AuthController@logout');
-//
-//// Registration Routes...
+
+// Registration Routes...
 //Route::get('register', 'Auth\AuthController@showRegistrationForm');
 //Route::post('register', 'Auth\AuthController@register');
-//
-//// Password Reset Routes...
+
+// Password Reset Routes...
 //Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
 //Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
 //Route::post('password/reset', 'Auth\PasswordController@reset');
-
 
 
 Route::resource('dashboard', 'HomeController');
